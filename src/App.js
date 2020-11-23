128a9602778a8fa669d98ccad87fda147b4beaa8
sharonrosa <srwijnberg@gmail.com>
2020-11-23 21:01:05 +0100

    this is first fruit assignment commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,58 @@
 import React from 'react';
+import Product from "./Product";
 import './App.css';
+import citroenen from './assets/lemons.jpg';
+import limoenen from './assets/limes.jpeg';
+import ijsklontjes from './assets/icecubes.jpeg';
+import { ReactComponent as Shoppingcart } from "./assets/winkelmandje.svg";
 
 function App() {
-  return (
-    <div>
-      Begin hier met de tutorial!
-    </div>
+
+    return (
+    <fragment>
+        <nav>
+            <ul>
+                <li><a href="/">Shop</a></li>
+                <li><a href="/">Ons verhaal</a></li>
+                <li><a href="/">Blog</a></li>
+            </ul>
+            <Shoppingcart className="shopping-cart-icon"/>
+        </nav>
+        <header>
+            <h1>Fruit perfection</h1>
+            <button
+                type="button"
+            onClick={() => console.log("Jij wil shoppen")
+            }
+            >
+                Shop nu
+            </button>
+        </header>
+    <main>
+        <Product
+                image={citroenen}
+                title="Citroen"
+                description="Een citroen is voor de meeste mensen te zuur om zo uit de hand te eten.
+                            Van citroen kun je het vruchtvlees het sap en de schil gebruiken. Het sappige, lichtgele zure vruchtvlees
+                            versterkt de smaak van ander voedsel."
+        />
+
+        <Product
+                image={limoenen}
+                title="Limoen"
+                description="Limoen is familie van de citroen en de sinaasappel en behoort tot de
+                citrusvruchten (Wijnruitfamilie). Limoenen zijn rond en kleiner dan citroenen. De schil is dun, vrij glad en groen."
+        />
+
+        <Product
+                image={ijsklontjes}
+                title="Ijsblokjes"
+                description="Een ijsblokje of ijsklontje is bevroren water in de vorm van een klein blokje.
+                Het wordt gemaakt in een diepvriezer door water in een plastic vorm te laten bevriezen."
+        />
+        
+    </main>
+    </fragment>
   );
 }
 
